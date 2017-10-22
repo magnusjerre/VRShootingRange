d691ed012daac22f08316c8de5b0fe13651adb26
Magnus Jerre <magnus.jerre@gmail.com>
2017-10-22 12:22:24 +0200

    Default implementaion for EmptyTrigger

--- Assets/Scripts/Triggers/EmptyTrigger.cs
+++ Assets/Scripts/Triggers/EmptyTrigger.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EmptyTrigger : ITriggerable
 {
+    protected List<IListener> _listeners;
+
+    public EmptyTrigger()
+    {
+        _listeners = new List<IListener>();
+    }
+    
     public void AddListener(IListener listener)
     {
-        
+        if (listener != null && !_listeners.Contains(listener))
+        {
+            _listeners.Add(listener);
+        }
     }
 
     public string Name()
@@ -15,10 +26,15 @@ public class EmptyTrigger : ITriggerable
     public void Trigger()
     {
         Debug.Log("Trigger empty trigger");
+        for (var i = 0; i < _listeners.Count; i++)
+        {
+            _listeners[i].Notify(this);
+        }
     }
 
     public void Notify(object notifier)
     {
         Debug.Log("Notify empty trigger");
+        Trigger();
     }
 }
