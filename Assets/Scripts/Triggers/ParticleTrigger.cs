30dfaf1826a9f963096868cb2013ef9707734e5d
Magnus Jerre <magnus.jerre@gmail.com>
2017-12-27 14:59:23 +0100

    Adding particles when popup targets are shown

---
+++ Assets/Scripts/Triggers/ParticleTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Jerre {
+	public class ParticleTrigger : BaseTriggerable
+	{
+
+		[SerializeField] private ParticleSystem particleSystemPrefab;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			
+		}
+
+		public override void Trigger() {
+			Debug.Log ("Trigger called");
+			var ps = Instantiate (particleSystemPrefab, transform);
+			var shape = ps.shape;
+			Invoke ("NotifyListeners", ps.main.duration);
+		}
+		
+	}
+}
